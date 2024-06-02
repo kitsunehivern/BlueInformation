6db2cbf85b3739db5a8e74dc0868513a7c41a009
Đinh Cao Minh Quân <minhquandinhcao@gmail.com>
2024-06-02 16:51:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/blueinformation/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.blueinformation;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.*;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        Configuration config = getResources().getConfiguration();
+        onConfigurationChanged(config);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.main_activity_landscape);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            setContentView(R.layout.main_activity_portrait);
+        }
+
+        Button buttonVoiceline = findViewById(R.id.ButtonVoiceline);
+        Button buttonTelephone = findViewById(R.id.ButtonTelephone);
+        Button buttonWebsite = findViewById(R.id.ButtonWebsite);
+
+        buttonVoiceline.setOnClickListener(this);
+        buttonTelephone.setOnClickListener(this);
+        buttonWebsite.setOnClickListener(this);
+
+        super.onConfigurationChanged(newConfig);
+    }
+
+    public void onClickVoiceline(View view) {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+        }
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.voiceline);
+        mediaPlayer.start();
+    }
+
+    public void onClickWebsite(View view) {
+        String url = "https://bluearchive.fandom.com/wiki/Takanashi_Hoshino";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
+
+    public void onClickTelephone(View view) {
+        String number = "tel:202-324-3000";
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(number));
+        startActivity(intent);
+    }
+
+    public void onClickButton(View view) {
+        if (view.getId() == R.id.ButtonVoiceline) {
+            onClickVoiceline(view);
+        } else if (view.getId() == R.id.ButtonTelephone) {
+            onClickTelephone(view);
+        } else if (view.getId()==R.id.ButtonWebsite) {
+            onClickWebsite(view);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        onClickButton(view);
+    }
+}
